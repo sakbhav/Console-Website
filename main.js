952e170c5f27648571c6c8234db71671d60f75db
Saket Srivastava <saket@srivastava.pw>
2020-03-15 10:24:55 +0530

    Fixing MIME type

--- main.js
+++ main.js
@@ -110,7 +110,6 @@ function main() {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
             var lines = this.responseText.split('\n');
             for (var i = 0; i < lines.length; i++) {
               term.writeln(lines[i]);
